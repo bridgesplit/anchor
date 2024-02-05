203f8f0162fc9ce733796ff531d0260ee61a70b7
Bhargava Sai Macha <mbsai29@gmail.com>
2024-02-05 22:06:40 +0530

    add group pointer udpate

--- spl/src/token_2022_extensions/group_pointer.rs
+++ spl/src/token_2022_extensions/group_pointer.rs
@@ -34,3 +34,29 @@ pub struct GroupPointerInitialize<'info> {
     pub token_program_id: AccountInfo<'info>,
     pub mint: AccountInfo<'info>,
 }
+
+pub fn group_pointer_update<'info>(
+    ctx: CpiContext<'_, '_, '_, 'info, GroupPointerUpdate<'info>>,
+    group_address: Option<Pubkey>,
+) -> Result<()> {
+    let ix = spl_token_2022::extension::group_pointer::instruction::update(
+        ctx.accounts.token_program_id.key,
+        ctx.accounts.mint.key,
+        ctx.accounts.authority.key,
+        &[&ctx.accounts.authority.key],
+        group_address,
+    )?;
+    solana_program::program::invoke_signed(
+        &ix,
+        &[ctx.accounts.token_program_id, ctx.accounts.mint],
+        ctx.signer_seeds,
+    )
+    .map_err(Into::into)
+}
+
+#[derive(Accounts)]
+pub struct GroupPointerUpdate<'info> {
+    pub token_program_id: AccountInfo<'info>,
+    pub mint: AccountInfo<'info>,
+    pub authority: AccountInfo<'info>,
+}
